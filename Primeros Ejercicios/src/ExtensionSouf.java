47804984990c9edaefaa6078a38ea50fd81e9d60
Adrián Rami <230053@fppiramide.com>
2024-09-24 11:10:18 +0200

    Ejercicio medias

---
+++ Primeros Ejercicios/src/ExtensionSouf.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ExtensionSouf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero para hacer la media: ");
+        double a = sc.nextDouble();
+
+        System.out.println("Ingrese el primer numero para hacer la media: ");
+        double b = sc.nextDouble();
+
+        System.out.println("Ingrese el primer numero para hacer la media: ");
+        double c = sc.nextDouble();
+
+        System.out.println("Ingrese el primer numero para hacer la media: ");
+        double d = sc.nextDouble();
+
+        double media = (a+b+c+d)/4;
+
+
+        System.out.printf("La media de los números %.2f %.2f %.2f y %.2f es igual a %f",a ,b,c,d, media);
+    }
+}
