63df24af8e833c656c3fffc51b700aa3e1c78817
Adrian <230053@fppiramide.com>
2024-09-27 10:03:33 +0200

    mathPow

---
+++ IfFor/src/mathPow.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/*
+Escribe un programa que indique si un número entero leído por teclado se puede expresar
+como el cuadrado de un número entero. Por ejemplo, el número 9 se puede
+expresar como el cuadrado de 3, mientras que el 8 no.
+(Pista, no hace falta utilizar repetición, utilizar librería matemática).
+ */
+public class mathPow {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numeroRepresentado = sc.nextInt();
+        double Raiz = Math.sqrt(numeroRepresentado);
+
+        if(numeroRepresentado == Math.pow(Raiz,2)){
+            System.out.println("La raíz es correcta el resultado es " + Raiz);
+        }else{
+            System.out.println("La raíz no devuelve un número entero");
+        }
+    }
+}
