4ead43bf02086b8df32d26dc0afec44b751ad155
Adrian <230053@fppiramide.com>
2024-09-30 12:30:20 +0200

    comienzo ejercicio 5

--- IfFor/src/ej5.java
+++ IfFor/src/ej5.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 /*
 Escribe un programa que solicite un carácter por teclado e informe de si es alfanumérico (letra o dígito) o no.
  En caso de ser alfanumérico deberá indicar si es una letra o un dígito.
  En caso de ser una letra deberá informar de si es minúscula o mayúscula.
  */
 public class ej5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char caracter = sc.next().charAt(0);
+        if(caracter>0 && caracter<10){
+            System.out.println(caracter+" es un caracter numérico");
+        }
+    }
 
 }
