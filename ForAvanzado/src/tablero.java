9abb0f4041dead3f6ff6d2af5b6a038f419c91c2
Adrian <230053@fppiramide.com>
2024-10-02 12:46:36 +0200

    Tablero

---
+++ ForAvanzado/src/tablero.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class tablero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 5;
+        char[][] tablero=new char[6][6];
+
+        char barco= 'b';
+        tablero[2][3] = barco;
+        tablero[4][5] = barco;
+//Tablero
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
