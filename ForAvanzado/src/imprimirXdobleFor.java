f1c63df0f88258e40a42c8824b39b6b426c5b253
Adrian <230053@fppiramide.com>
2024-10-03 12:47:47 +0200

    tablero complejo

---
+++ ForAvanzado/src/imprimirXdobleFor.java
@@ -0,0 +1,25 @@
+/*
+Haz un programa que pinte una x
+ */
+public class imprimirXdobleFor {
+    public static void main(String[] args) {
+        //Utilizando switch case por primera vez.
+
+        final int dimension = 10;
+
+        for (int i = 1; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+
+             //   System.out.print("x\t");
+
+                if (j == i || i + j == dimension) {
+                 System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+           System.out.println();
+        }
+    }
+}
