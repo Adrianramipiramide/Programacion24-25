d0de3941cac34f0adbf8359b6493dad51d4eb6c1
Adrian <230053@fppiramide.com>
2024-10-08 13:49:36 +0200

    hundir la flota practicamente terminado y funcionando sin arrays

--- Arrays/src/HundirFlota.java
+++ Arrays/src/HundirFlota.java
@@ -3,27 +3,35 @@ import java.util.Scanner;
 public class HundirFlota {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Bienvenido al hundir la flota");
+        System.out.println("Bienvenido al hundir la flota\n");
         boolean ganar = false;
-        final int TamanoTablero = 8;
-        final int intentos = 2;
+        System.out.println("De que tamaño quieres el tablero");
+        final int TamanoTablero = sc.nextInt();
+        final int posicionMax = TamanoTablero-1;
+        System.out.println("\n Cuantos intentos quieres tener");
+        final int intentos = sc.nextInt();
 
-        try {
-            for (int fila = 0; fila < TamanoTablero; fila++) {
-                for (int columna = 0; columna < TamanoTablero; columna++) {
-                    System.out.print("x\t");
-                }
-                System.out.println();
-            }
-        } catch (Exception e) {
-            System.out.println("El caracter introducido no es correcto");
-        }
-        System.out.println("En que posición quieres colocar tu barco");
-        int PosicionBarco = sc.nextInt();
 
         for (int fila = 0; fila < TamanoTablero; fila++) {
             for (int columna = 0; columna < TamanoTablero; columna++) {
-                if (PosicionBarco == fila && PosicionBarco == columna) {
+                System.out.print("x\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nEn que fila quieres colocar tu barco");
+        int PosicionBarco = sc.nextInt();
+        if (PosicionBarco > posicionMax) {
+            PosicionBarco = posicionMax;
+        }
+        System.out.println("En que columna quieres colocar tu barco");
+        int PosicionBarcoC = sc.nextInt();
+        if (PosicionBarcoC > posicionMax) {
+            PosicionBarcoC = posicionMax;
+        }
+        for (int fila = 0; fila < TamanoTablero; fila++) {
+            for (int columna = 0; columna < TamanoTablero; columna++) {
+                if (PosicionBarco == fila && PosicionBarcoC == columna) {
                     System.out.print("b\t");
                 } else {
                     System.out.print("x\t");
@@ -31,27 +39,38 @@ public class HundirFlota {
             }
             System.out.println();
         }
-        System.out.println("El barco está en la posicion " + PosicionBarco + ", " + PosicionBarco);
+        System.out.println("El barco está en la posicion " + PosicionBarco + ", " + PosicionBarcoC);
 
 
-        System.out.println("\nComineza el juego");
-        System.out.println("Tienes 3 intentos para derribar el barco");
-        System.out.println("En que posición crees que esta el barco");
+        System.out.println("\n\n\nComineza el juego");
+        System.out.println("Tienes " + intentos + " intentos para derribar el barco");
+
         for (int i = 0; i < intentos; i++) {
-            int fuego = sc.nextInt();
-            if (fuego == PosicionBarco) {
+            System.out.println("En que fila crees que esta el barco");
+            int fuegof = sc.nextInt();
+            if (fuegof > posicionMax) {
+                fuegof = posicionMax;
+            }
+            System.out.println("En que columna crees que esta el barco");
+            int fuegoc = sc.nextInt();
+            if (fuegoc > posicionMax) {
+                fuegoc = posicionMax;
+            }
+            if (fuegof == PosicionBarco && fuegoc == PosicionBarcoC) {
                 System.out.println("Has derribado el barco que te faltaba");
                 for (int fila = 0; fila < TamanoTablero; fila++) {
                     for (int columna = 0; columna < TamanoTablero; columna++) {
-                        if (fila == PosicionBarco && columna == PosicionBarco) {
+                        if (fila == PosicionBarco && columna == PosicionBarcoC) {
                             System.out.print("b\t");
                             ganar = true;
                         } else {
                             System.out.print("x\t");
                         }
+
                     }
                     System.out.println();
                 }
+                break;
             } else {
                 System.out.println("agua");
             }
