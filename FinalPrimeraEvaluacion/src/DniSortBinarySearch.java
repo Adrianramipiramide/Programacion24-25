38ae24a68739cd7db08e8967bcf6ad37b97b0f60
Adrián Rami <230053@fppiramide.com>
2024-10-22 13:51:50 +0200

    cola ejercicio

--- FinalPrimeraEvaluacion/src/DniSortBinarySearch.java
+++ FinalPrimeraEvaluacion/src/DniSortBinarySearch.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 import static java.lang.Math.random;
 
 /*
@@ -6,27 +8,49 @@ número del DNI en orden de llegada, para posteriormente, atenderles.
  */
 public class DniSortBinarySearch {
     public static void main(String[] args) {
-        final int Dimension = 50;
-        String dni;
-        String[]clientes= new String[Dimension];
+        final int Dimension = 3;
+        int posicionPersona = 0;
 
-        llenarArray(clientes);
-    }
-    
-    public static void ponerALaCola(String[] personasDNI){
+        String[] clientes = new String[Dimension];
 
-    }
+        ponerALaCola(clientes, Dimension, posicionPersona);
+        mostrarCola(clientes, posicionPersona, Dimension);
+        sacarDeCola(clientes, Dimension, posicionPersona);
 
-    public static void  sacarDeCola(String[] personasDNI){
 
     }
 
-    public static void llenarArray(String[] clienteCola){
+    //Lleno el array poniendo a las personas en la cola
+    public static void ponerALaCola(String[] personasDNI, final int personasEnCola, int posicionPersona) {
 
-        for (int i = 0; i < clienteCola.length ; i++) {
-          //  String[i]  clienteCola  =  (Math.random()*10);
+        for (int i = 0; i < personasEnCola; i++) {
+            personasDNI[i] = JOptionPane.showInputDialog("Introduce el DNI de la persona que acab de llegar a la cola");
         }
+    }
 
+    //muestro el array con los DNI y las posiciones
+    public static void mostrarCola(String[] personasDNI, int posicionPersonas, final int Dimension) {
+        System.out.println();
+        for (int i = 0; i < Dimension; i++) {
+            posicionPersonas++;
+            System.out.println("La persona con el DNI " + personasDNI[i] + " se encuntra en la posicion " + posicionPersonas);
+        }
+    }
 
+    public static void sacarDeCola(String[] personasDNI, final int Dimension, int posicionPersona) {
+
+        String input = JOptionPane.showInputDialog("Cuantas personas quieres sacar de la cola");
+        int personasqueSacas = Integer.parseInt(input);
+        System.out.println("\nPersonas que han salido de la cola");
+        for (int i = 0; i < personasqueSacas; i++) {
+            if (personasqueSacas > Dimension) {
+                break;
+            }
+
+            System.out.println("La persona con el DNI "+personasDNI[i] + " ha salido de la cola\n");
+        }
+        for (int i = personasqueSacas; i < Dimension; i++) {
+            System.out.println("La persona con el DNI "+ personasDNI[i]+ " se encuentrá en la posición "+ (i+1-personasqueSacas));
+        }
     }
 }
