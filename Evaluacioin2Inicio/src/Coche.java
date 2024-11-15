7a932da1639fb93f866d9de4ef2c44cba3d90b86
Adrian <230053@fppiramide.com>
2024-11-15 10:29:27 +0100

    POO inicios

--- Evaluacioin2Inicio/src/Coche.java
+++ Evaluacioin2Inicio/src/Coche.java
@@ -8,12 +8,14 @@ public class Coche {
     private String modelo;
     private int velocidad;
     private int acelerar;
+    private static int toatalCoches = 0;
 
     public Coche(String color, String modelo) {
         this.color = color;
         this.modelo = modelo;
         this.velocidad = 0;
-        this.acelerar  = +15;
+        this.acelerar = +15;
+        toatalCoches++;
     }
 
     public void setColor(String color) {
@@ -26,28 +28,40 @@ public class Coche {
         return velocidad;
     }
 
-    public int aceleracionConcreta(){
+    public int aceleracionConcreta() {
         this.velocidad += acelerar;
         return velocidad;
     }
 
     public int frenar() {
-        if(velocidad<10){
-            this.velocidad=velocidad;
-        }else {
+        if (velocidad < 10) {
+            this.velocidad = velocidad;
+        } else {
             this.velocidad -= 10;
         }
 
         return velocidad;
     }
 
-    public int getVelocidad(){
+    public int getVelocidad() {
         System.out.println(velocidad);
         return velocidad;
     }
 
-    public void setVelocidad(int velocidad){
+    public void setVelocidad(int velocidad) {
         this.velocidad = velocidad;
+    }
+
+    public static int convertirAKm(int velocidad) {
+        int kilometros = velocidad * 1609;
+        System.out.printf("La velocidad del coche es %d \n", kilometros / 1000);
+        return kilometros;
+    }
+
+    public static int totalCoche() {
+
+        System.out.printf("El numero total de coches es %d",toatalCoches);
+        return toatalCoches;
     }
 
     @Override
--- Evaluacioin2Inicio/src/MainCoche.java
+++ Evaluacioin2Inicio/src/MainCoche.java
@@ -1,6 +1,8 @@
 public class MainCoche {
     public static void main(String[] args) {
         Coche coche1 = new Coche("Verde", "Urus");
+        Coche coche2 = new Coche("Amarillo", "CX5");
+        Motor motor1 = new Motor("X", 120);
         coche1.acelerar();
         coche1.frenar();
         coche1.acelerar();
@@ -12,7 +14,8 @@ public class MainCoche {
         coche1.acelerar();
         coche1.getVelocidad();
         System.out.println(coche1);
-
-
+        //Velocidad en km
+        Coche.convertirAKm(coche1.getVelocidad());
+        Coche.totalCoche();
     }
 }
