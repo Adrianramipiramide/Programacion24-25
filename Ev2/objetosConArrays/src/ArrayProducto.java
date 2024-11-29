f88112e39f458a7b30835835e801666c205573d1
Adrian <230053@fppiramide.com>
2024-11-29 10:27:41 +0100

    ejercicios vvarios

--- Ev2/objetosConArrays/src/ArrayProducto.java
+++ Ev2/objetosConArrays/src/ArrayProducto.java
@@ -5,11 +5,11 @@ public class ArrayProducto {
     ArrayList<Producto> listaProductos ;
 
     public static void agregarProducto(Producto palabra, ArrayList<String> lista){
-        lista.add(String.valueOf(palabra));
+        lista.add(palabra.getNombre());
     }
 
-    public static void eliminarProducto(String palabra, ArrayList<String> lista){
-        lista.remove(palabra);
+    public static void eliminarProducto(Producto palabra, ArrayList<String> lista){
+        lista.remove(palabra.getNombre());
     }
 
     public static void mostrarProducto(int posicion,ArrayList<String> lista){
