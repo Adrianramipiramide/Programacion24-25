d55cf595a3ec1cd905c4025f5580c3c4ec5b8e67
Adrian <230053@fppiramide.com>
2025-03-19 10:55:47 +0100

    ex

--- Ev3/Streamss/src/main/java/RegEX.java
+++ Ev3/Streamss/src/main/java/RegEX.java
@@ -3,9 +3,9 @@ import java.util.regex.Pattern;
 
 public class RegEX {
     public static void main(String[] args) {
-        String prueba = "4";
+        String prueba = "5";
 
-        Pattern p = Pattern.compile("[4,6]");
+        Pattern p = Pattern.compile("[4-6]");
         Matcher m = p.matcher(prueba);
         boolean coincide = m.matches();
         System.out.println(coincide);
